1ad9f2ef8a872a77a417f1b82a8d77d0d2654d97
shrvya <shrivyashetty@gmail.com>
2021-08-22 10:58:24 +0530

    view code

--- linecomparisn.java
+++ linecomparisn.java
@@ -1,9 +1,24 @@
 package linecomparisn;
 
+import java.util.Scanner;
+
 public class linecomparisn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("welcome to line comparison");
+		
+		int x1,x2,y1,y2;//2 end points of line 1
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the values for line 1");
+		x1=sc.nextInt();
+		x2=sc.nextInt();
+		y1=sc.nextInt();
+		y2=sc.nextInt();
+		double lengthline1;
+		lengthline1 =Math.sqrt((Math.pow((x2-x1), 2))+(Math.pow((y2-y1), 2)));//find length of line
+		System.out.println("lenth of line 1 is :"+lengthline1);
 
 	}
 
